40be316c6474098e6a99ac3c47543802adbbe27d
sking-007 <106514081+sking-007@users.noreply.github.com>
2023-08-06 20:03:23 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://54.66.6.153:3000",
     credentials: true,
   },
 });
